6b88539f454da769faf3742e4c543c3ee4dea16a
Moises96 <lilmoises6996@gmail.com>
2015-02-03 17:32:26 -0500

    multiplos

--- 1_metodos/clase/Multiplos/solucion_2.py
+++ 1_metodos/clase/Multiplos/solucion_2.py
@@ -1,8 +1,7 @@
 suma = 0
-i = 1
-while i < 1000:
+i = 0
+for i in range(1000):
 	if i%3 == 0 or i%5 ==0:
 		suma += i
-	i += 1
 
 print suma
--- 1_metodos/clase/cuadrados/solucion_1.py
+++ 1_metodos/clase/cuadrados/solucion_1.py
@@ -2,3 +2,4 @@ x = 0
 for i in range(1,101):
 	x += i**2
 print x - 10000
+
