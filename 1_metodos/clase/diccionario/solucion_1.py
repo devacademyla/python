fe90a73922c007caefe3ef5983207f7d85008a7b
Moises96 <lilmoises6996@gmail.com>
2015-02-05 15:36:26 -0500

    ejercicio diccionario

---
+++ 1_metodos/clase/diccionario/solucion_1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+lista = [("rojo", "red"), ("verde", "green"), ("blanco", "white"), ("azul", "blue"), ("amarillo", "yellow")]
+diccionario = {
+	lista[0][0]:lista[0][1],
+	lista[1][0]:lista[1][1],
+	lista[2][0]:lista[2][1],
+	lista[3][0]:lista[3][1],
+	lista[4][0]:lista[4][1],
+}
+print diccionario
