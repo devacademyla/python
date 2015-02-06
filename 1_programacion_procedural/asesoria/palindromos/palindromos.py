929d6356a27574cc5c571a4a96e96494a94445b5
Moises96 <lilmoises6996@gmail.com>
2015-02-05 22:51:52 -0500

    asesoria

---
+++ 1_programacion_procedural/asesoria/palindromos/palindromos.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+cadena = raw_input("Introduce la cadena: ")
+cont = 0
+cadena2 = ""
+es = True
+ 
+for i in cadena:
+	if i.isupper():
+		i = i.lower()
+	if i != " ":
+		cadena2 = cadena2 + i
+		cont = cont + 1
+ 
+for i in range(cont/2):
+	if cadena2[i] != cadena2[cont-i-1]:
+		es = False
+		break
+ 
+if es == True:
+	print "Es palíndromo"
+else:
+	print "No es palíndromo"
