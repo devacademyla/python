4e8f6b7f1c711e7ad235760e06d2f8a88121976f
JEnriquePS <jenrique.ps@gmail.com>
2015-02-12 10:52:16 -0500

    ejercicio semana 2

---
+++ 2_programacion_funcional/clase/Suma de num. Primos/suma_primos.py
@@ -0,0 +1,14 @@
+import math
+
+
+def primo(num):
+    i = 2
+    if num > 1:
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                return False
+        return num
+    else:
+        return False
+lista = range(1, 10000)
+print sum(filter(primo, lista))
