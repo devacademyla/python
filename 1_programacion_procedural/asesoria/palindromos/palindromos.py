55749b6cb322dafdc76f230179f71379c55db063
Antonio Ognio <antonio@ognio.com>
2015-02-25 06:56:18 -0500

    Cambios por PEP-8 y otras mejoras

--- 1_programacion_procedural/asesoria/palindromos/palindromos.py
+++ 1_programacion_procedural/asesoria/palindromos/palindromos.py
@@ -5,18 +5,18 @@ cadena2 = ""
 es = True
  
 for i in cadena:
-	if i.isupper():
-		i = i.lower()
-	if i != " ":
-		cadena2 = cadena2 + i
-		cont = cont + 1
+    if i.isupper():
+        i = i.lower()
+    if i != " ":
+        cadena2 = cadena2 + i
+        cont = cont + 1
  
 for i in range(cont/2):
-	if cadena2[i] != cadena2[cont-i-1]:
-		es = False
-		break
+    if cadena2[i] != cadena2[cont-i-1]:
+        es = False
+        break
  
 if es == True:
-	print "Es palíndromo"
+    print "Es palíndromo"
 else:
-	print "No es palíndromo"
+    print "No es palíndromo"
--- 1_programacion_procedural/asesoria/todito/todito.py
+++ 1_programacion_procedural/asesoria/todito/todito.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
+
 import random
 
 def lanzar_dado():
-	return 1 + random.randrange(6)
+    return 1 + random.randrange(6)
 
 def definir_castigo(dado):
-	if dado == 1 or dado == 5:
-		print "Toman todos"
-	if dado == 2 or dado == 6:
-		print "Toma otro"
-	if dado == 3:
-		print "Toma el de la derecha"
-	if dado == 4:
-		print "Toma el de la izquierda"
+    if dado == 1 or dado == 5:
+        print "Toman todos"
+    if dado == 2 or dado == 6:
+        print "Toma otro"
+    if dado == 3:
+        print "Toma el de la derecha"
+    if dado == 4:
+        print "Toma el de la izquierda"
 
 def jugar():
-	dado = lanzar_dado()
-	print "Salió el valor", dado, "entonces:"
-	castigo = definir_castigo(dado)
+    dado = lanzar_dado()
+    print u"Salió el valor", dado, u"entonces:"
+    castigo = definir_castigo(dado)
 
-jugar()
+if __name__ == '__main__':
+    jugar()
