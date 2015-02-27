5600a2cde3ee7cfa5c2434b9d966eb6660c212d8
JEnriquePS <jenrique.ps@gmail.com>
2015-02-26 19:56:25 -0500

    ejecicio asesoria modificado

--- 3_poo/asesoria/ingeniero/ingeniero.py
+++ 3_poo/asesoria/ingeniero/ingeniero.py
@@ -12,7 +12,6 @@ class Dado(object):
         self.lista = []
         for i in range(n):
             self.lista += [self.lanzar_dado()]
-        print self.lista
         return self.lista
 
 
@@ -22,21 +21,35 @@ class Ingeniero(Dado):
         """ La funcion magica __init__ se ejecuta al llama la
         clase Ingeniero, ejecutándose la funcion resultado """
         super(Ingeniero, self).__init__(*args, **kwargs)
+        self.n = 5
+        self.list_numeros = self.num_dados(self.n)
+        self.num_escogido = []
+        self.escoger()
         self.resultado()
 
     def escoger(self):
-        self.preguntas = [
-            u'Primer número: ', u'Sumar el número: ',
-            u'Restar el número: ', u'Multiplicar el número: ',
-            u'Dividir el número: ']
-        return self.preguntas
+        print self.list_numeros
+
+        preguntas = [
+            u'Primer numero: ', u'Sumar el numero: ',
+            u'Restar el numero: ', u'Multiplicar el numero: ',
+            u'Dividir el numero: ']
+
+        for pregunta in preguntas:
+            num = False
+            while num is False:
+                num = int(raw_input(pregunta))
+                if num in self.list_numeros:
+                    self.list_numeros.remove(num)
+                    self.num_escogido.append(num)
+                else:
+                    num = False
+                    print 'El número ingresado no se encuentra.'
+
+        return self.num_escogido
 
     def resultado(self):
-        n = 5
-        dados = self.num_dados(n)
-        pregunta = self.escoger()
-        for i in range(0, n):
-            print pregunta[i], dados[i]
+        dados = self.num_escogido
         resultado = dados[0] + dados[1]
         resultado -= dados[2]
         resultado *= float(dados[3])
